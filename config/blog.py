6b14d940849bb00e59fde16be670946f61b77d25
Stefano Borini <stefano.borini@gmail.com>
2014-10-05 17:20:22 +0200

    common

--- config/blog.py
+++ config/blog.py
@@ -1,54 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+import os
+import sys
+sys.path.append(os.path.dirname(__file__))
+from common import *
 
-AUTHOR = u'Stefano Borini'
 SITENAME = u'ForTheScience.org'
 SITESUBTITLE= u"A blog about Scientific Programming"
 SITEURL = 'http://forthescience.org/blog'
-ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
-ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
-
 PATH = 'content'
-PAGE_PATHS = ['pages']
-PAGE_URL = '{slug}'
-PAGE_SAVE_AS = '{slug}/index.html'
-
-ARTICLE_PATHS = ['posts']
-
-STATIC_PATHS = ['wp-content/uploads']
-WITH_FUTURE_DATES = False
-GITHUB_URL="http://github.com/stefanoborini/"
-GOOGLE_ANALYTICS="UA-13239309-1"
-EXTRA_PATH_METADATA = {}
-TIMEZONE = 'Europe/Rome'
-FILENAME_METADATA='(?P<date>\d{4}-\d{2}-\d{2})_(?P<slug>.*)'
-DEFAULT_LANG = u'en'
-
-# Feed generation is usually not desired when developing
-FEED_ATOM = None
-FEED_MAX_ITEMS = 15
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-
-# Blogroll
-LINKS = (
-         ('GitHub Account', 'http://github.com/stefanoborini/'),
-         ('LinkedIn Account', 'http://linkedin.com/in/stefanoborini'),
-         ('Reddit Links', 'http://reddit.com/r/esbio'),
-         ('My Whatever Blog', 'http://forthescience.org/whatever'),
-         ('My Gaia Blog', 'http://forthescience.org/gaia'),
-         )
-
-# Social widget
-SOCIAL = ()
-PLUGIN_PATHS = ['../plugins']
-PLUGINS = [] #["googleplus_comments"]
-
-DEFAULT_PAGINATION = 10
-DISPLAY_CATEGORIES_ON_MENU = False
-DISPLAY_TAGS_ON_SIDEBAR = False
 THEME = "themes/blog/"
-# Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = False
-DISQUS_SITENAME = 'forthescience'
